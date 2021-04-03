3ed118c3778a21384c6b939771d8dc42fbaed483
Cristopher Scott <crisscottmendoza@gmail.com>
2021-04-02 22:20:40 -0600

    Final Changes

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
+import os
 from flask import Flask, jsonify, request, render_template, redirect, url_for, abort, Response
 from werkzeug.urls import url_parse
+from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
 import json
 from datetime import datetime
+import pytz
+from tzlocal import get_localzone
+
+UPLOAD_FOLDER = './static/img/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost:5432/diary'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 db = SQLAlchemy(app)
 
@@ -20,7 +28,6 @@ def index():
 
 @app.route('/getinfo', methods=['GET', 'POST'])
 def get_data():
-    # GET request
     if request.method == 'GET':
         json_data_list = []
         entries = Entry.get_all()
@@ -42,84 +49,38 @@ def get_data():
         
         return jsonify(json_data_list)
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_form():
 
+    dir_file = ''
+    if request.files:
+        file = request.files['file']
+    
+        if file:
+            filename, file_extension = os.path.splitext(file.filename)
+            filename = str(datetime.timestamp(datetime.now(pytz.timezone("Mexico/General")))) + file_extension.lower()
+            dir_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(dir_file)
+    
+    query = Entry(date = request.form['date'], feel = request.form['feel'], description = request.form['description'], image = dir_file, timestamp = datetime.now(pytz.timezone("Mexico/General")))
+    if query.save():
+        return jsonify({'status': 200}) 
+    else:
+        return jsonify({'status': 500}) 
 
+@app.route('/delete')
+def delete_element(): 
+    id = request.args.get('element')
+    element = Entry.get_by_id(id)
 
-
-
-
-
-
-# @app.route('/p/<string:slug>/')
-# def show_post(slug):
-#     post = Post.get_by_slug(slug)
-#     if post is None:
-#         abort(404)
-#     return render_template("post_view.html", post=post)
-
-# @app.route("/admin/post/", methods=['GET', 'POST'], defaults={'post_id': None})
-# @app.route("/admin/post/<int:post_id>/", methods=['GET', 'POST'])
-# @login_required
-# def post_form(post_id):
-#     form = PostForm()
-#     if form.validate_on_submit():
-#         title = form.title.data
-#         content = form.content.data
-#         post = Post(user_id=current_user.id, title=title, content=content)
-#         post.save()
-#         return redirect(url_for('index'))
-#     return render_template("admin/post_form.html", form=form)
-
-# @app.route("/signup/", methods=["GET", "POST"])
-# def show_signup_form():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     form = SignupForm()
-#     error = None
-#     if form.validate_on_submit():
-#         name = form.name.data
-#         email = form.email.data
-#         password = form.password.data
-#         # Comprobamos que no hay ya un usuario con ese email
-#         user = User.get_by_email(email)
-#         if user is not None:
-#             error = f'El email {email} ya está siendo utilizado por otro usuario'
-#         else:
-#             # Creamos el usuario y lo guardamos
-#             user = User(name=name, email=email)
-#             user.set_password(password)
-#             user.save()
-#             # Dejamos al usuario logueado
-#             login_user(user, remember=True)
-#             next_page = request.args.get('next', None)
-#             if not next_page or url_parse(next_page).netloc != '':
-#                 next_page = url_for('index')
-#             return redirect(next_page)
-#     return render_template("signup_form.html", form=form, error=error)
-
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.get_by_id(int(user_id))
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.get_by_email(form.email.data)
-#         if user is not None and user.check_password(form.password.data):
-#             login_user(user, remember=form.remember_me.data)
-#             next_page = request.args.get('next')
-#             if not next_page or url_parse(next_page).netloc != '':
-#                 next_page = url_for('index')
-#             return redirect(next_page)
-#     return render_template('login_form.html', form=form)
-
-# @app.route('/logout')
-# def logout():
-#     logout_user()
-#     return redirect(url_for('index'))
+    if element.image != "":
+        if os.path.isfile(element.image):
+            os.remove(element.image)
+    
+    if Entry.delete_by_id(id):
+        return jsonify({'status': 200}) 
+    else:
+        return jsonify({'status': 500})     
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,93 +1,82 @@
-// CARGAMOS LA INFORMACIÓN A MOSTRAR EN CUANDO LA PAGINA CARGUE 
-function load_info(callback) {
-    fetch('/getinfo').then(function (response) {
-        return response.json();
-    }).then(function (text) {
-        if(text) {
-
-            let list_diary = document.getElementById('list_diary'); 
-            list_diary.innerHTML = "";
-
-            text.forEach(function (item, index) {
-
-                // CREATE ELEMENTS
-                let li = document.createElement('li'),
-                a = document.createElement('a'),
-                div = document.createElement('div'),
-                span1 = document.createElement('span'),
-                span2 = document.createElement('span'),
-                p = document.createElement('p'), 
-                small = document.createElement('small'),
-                // CREATE ELEMENTS
-                // CREATE TEXT NODE
-                feel = document.createTextNode(item.feel),
-                description = document.createTextNode(item.description),
-                date = document.createTextNode(item.date);
-                // CREATE TEXT NODE
-
-                // ADD CLASSES
-                div.classList.add('list_diary_title');
-                span2.classList.add('option_delete');
-                // ADD CLASSES
-
-                // ADD DATA ATTRIBUTE IN ELEMENT
-                span2.dataset.element_id = item.id
-                // ADD DATA ATTRIBUTE IN ELEMENT
-
-                // CREATE IMG NODE IF EXISTS
-                let img;
-                if(item.image != null) {
-                    img = document.createElement('img');
-                    img.src = item.image;
-                    img.style.width = '25em';
-                }
-                // CREATE IMG NODE IF EXISTS
-
-                // APPEND ALL ELEMENTS
-                p.appendChild(description);
-                span1.appendChild(feel);
-                span2.textContent = 'x';
-                small.appendChild(date);
-
-                div.appendChild(span1);
-                div.appendChild(span2);
-
-                a.appendChild(div);
-                a.appendChild(p);
-                if(img != null) {
-                    a.appendChild(img);
-                }
-
-                a.appendChild(small);
-
-                li.appendChild(a);
-                list_diary.appendChild(li);
-                // APPEND ALL ELEMENTS
-
-            });
-        }
-
-        callback();
-    });
-}
-
-// CREATE EVENT ON CLICK TO DELETE ELEMENT
-function createEventDelete () {
-    var option_delete = document.getElementsByClassName('option_delete');
-    for (var i = 0; i < option_delete.length; i++) {
-        option_delete[i].addEventListener('click', deleteNote, false);
-    }
-}
-
-var deleteNote = function () {
-    var element_id = this.getAttribute('data-element_id');
-
-    console.log(element_id);
-}
-// CREATE EVENT ON CLICK TO DELETE ELEMENT
-
 window.addEventListener('load', function () { 
+
+    document.getElementById('create_date').value = getDate();
+
+    // LOAD INFORMATION OF INDEX PAGE
+    function load_info(callback) {
+        fetch('/getinfo').then(function (response) {
+            return response.json();
+        }).then(function (text) {
+            if(text) {
+
+                let list_diary = document.getElementById('list_diary'); 
+                list_diary.innerHTML = "";
+
+                text.forEach(function (item, index) {
+
+                    // CREATE ELEMENTS
+                    let li = document.createElement('li'),
+                    a = document.createElement('a'),
+                    div = document.createElement('div'),
+                    span1 = document.createElement('span'),
+                    span2 = document.createElement('span'),
+                    p = document.createElement('p'), 
+                    small = document.createElement('small'),
+                    // CREATE ELEMENTS
+                    // CREATE TEXT NODE
+                    feel = document.createTextNode(item.feel),
+                    description = document.createTextNode(item.description),
+                    date = document.createTextNode(item.date);
+                    // CREATE TEXT NODE
+
+                    // ADD CLASSES
+                    div.classList.add('list_diary_title');
+                    span2.classList.add('option_delete');
+                    // ADD CLASSES
+
+                    // ADD DATA ATTRIBUTE IN ELEMENT
+                    span2.dataset.element_id = item.id
+                    // ADD DATA ATTRIBUTE IN ELEMENT
+
+                    // CREATE IMG NODE IF EXISTS
+                    let img;
+                    if(item.image != null) {
+                        img = document.createElement('img');
+                        img.src = item.image;
+                        img.style.width = '25em';
+                    }
+                    // CREATE IMG NODE IF EXISTS
+
+                    // APPEND ALL ELEMENTS
+                    p.appendChild(description);
+                    span1.appendChild(feel);
+                    span2.textContent = 'x';
+                    small.appendChild(date);
+
+                    div.appendChild(span1);
+                    div.appendChild(span2);
+
+                    a.appendChild(div);
+                    a.appendChild(p);
+                    if(img != null) {
+                        a.appendChild(img);
+                    }
+
+                    a.appendChild(small);
+
+                    li.appendChild(a);
+                    list_diary.appendChild(li);
+                    // APPEND ALL ELEMENTS
+
+                });
+            }
+
+            callback();
+        });
+    }
+
     load_info(createEventDelete); 
+    // LOAD INFORMATION OF INDEX PAGE
 
     // CREATE EVENT ON CHANGE IMG
     var create_img = document.getElementById('create_img');
@@ -95,19 +84,195 @@ window.addEventListener('load', function () {
         loadFile(event);
     })
     // CREATE EVENT ON CHANGE IMG
-})
 
-var loadFile = function(event) {
-    var output = document.getElementById('create_img_preview');
-    if(event.target.files[0]) {
-        output.src = URL.createObjectURL(event.target.files[0]);
-        output.onload = function() {
-        URL.revokeObjectURL(output.src) // free memory
-        output.style.width = '100%';
-        output.style.marginTop = '5px';
+    // CREATE IMAGE PREVIEW
+    var loadFile = function(event) {
+        var output = document.getElementById('create_img_preview');
+        if(event.target.files[0]) {
+            output.src = URL.createObjectURL(event.target.files[0]);
+            output.onload = function() {
+                URL.revokeObjectURL(output.src) // free memory
+                output.style.width = '300px';
+                output.style.marginTop = '5px';
+            }
+        }
+        else {
+            output.src = "";
+        }
+    };
+    // CREATE IMAGE PREVIEW
+
+    // CREATE MASK FOR DATE
+    let create_date = document.getElementById('create_date');
+    create_date.addEventListener('keypress', function (e) {
+        if(e.keyCode < 47 || e.keyCode > 57) {
+            e.preventDefault();
+        }
+          
+        var len = this.value.length;
+
+        if(len !== 1 || len !== 3) {
+            if(e.keyCode == 47) {
+                e.preventDefault();
+            }
+        }
+
+        if(len === 2) {
+            this.value += '/';
+        }
+
+        if(len === 5) {
+            this.value += '/';
+        }
+    });
+    // CREATE MASK FOR DATE
+
+
+    // VALIDATE FORM BEFORE SEND
+    const form = document.getElementById('form');
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        let validate = true;
+
+        let elements = document.getElementsByClassName('form-option');
+        for (var i = 0; i < elements.length; i++) {
+            let data = elements[i].dataset.required;
+            if(elements[i].parentNode.getElementsByClassName('form-error')[0]) {
+                elements[i].parentNode.getElementsByClassName('form-error')[0].style.display = 'none';
+            }
+
+            if(data === 'true') {
+                let valor = elements[i].value;
+                if(!valor) {
+                    validate = false; 
+
+                    elements[i].parentNode.getElementsByClassName('form-error')[0].style.display = 'block';
+                    elements[i].parentNode.getElementsByClassName('form-error')[0].textContent = 'Dato Requerido';
+                }
+            }
+        }
+
+        if(!validate) {
+            return false; 
+        }
+
+        let create_date = document.getElementById('create_date');
+        var date = create_date.value;
+        var newdate = date.split("/").reverse().join("-");
+
+        if(!isValidDate(newdate)) {
+            create_date.parentNode.getElementsByClassName('form-error')[0].style.display = 'block';
+            create_date.parentNode.getElementsByClassName('form-error')[0].textContent = 'Verifique el formato de la fecha';
+
+            return false; 
+        }
+        
+        // CREATE FORMDATA
+        const file = document.getElementById('create_img').files[0];
+        const formdata = new FormData();
+
+        formdata.append('file', file);
+        formdata.append('date', document.getElementById('create_date').value);
+        formdata.append('feel', document.getElementById('create_feel').value);
+        formdata.append('description', document.getElementById('create_description').value);
+
+        fetch('/upload', {
+            method: 'POST', 
+            body: formdata
+        }).then((response) => {
+            const alert = document.getElementsByClassName('alert')[0];
+            alert.style.display = "block";
+            if(response.status == '200') {
+                alert.classList.add('success');
+                alert.classList.remove('danger');
+                alert.getElementsByClassName('text')[0].textContent = 'Nota Guardada Correctamente';
+            }
+            else {
+                alert.classList.remove('success');
+                alert.classList.add('danger');
+                alert.getElementsByClassName('text')[0].textContent = 'Ocurrió un error al almacenar la nota';
+            }
+
+            setTimeout(function(){ alert.style.display = "none"; }, 3600);
+
+            document.getElementById("form").reset();
+            document.getElementById('create_img_preview').src = "";
+            document.getElementById('create_date').value = getDate();
+            load_info(createEventDelete); 
+        });
+    })
+    // VALIDATE FORM BEFORE SEND
+
+    // CREATE EVENT ON CLICK TO DELETE ELEMENT
+    function createEventDelete () {
+        var option_delete = document.getElementsByClassName('option_delete');
+        for (var i = 0; i < option_delete.length; i++) {
+            option_delete[i].addEventListener('click', deleteNote, false);
         }
     }
-    else {
-        output.src = "";
+
+    var deleteNote = function () {
+        var element_id = this.getAttribute('data-element_id');
+
+        var action = confirm("La nota será eliminada ¿Está Seguro?");
+        if (action == true) {
+            fetch(`/delete?element=${element_id}`)
+            .then(function (response) {
+                return response.json();
+            }).then(function (response) {
+                const alert = document.getElementsByClassName('alert')[0];
+                alert.style.display = "block";
+                if(response.status == '200') {
+                    alert.classList.add('success');
+                    alert.classList.remove('danger');
+                    alert.getElementsByClassName('text')[0].textContent = 'Nota Eliminada Correctamente';
+                }
+                else {
+                    alert.classList.remove('success');
+                    alert.classList.add('danger');
+                    alert.getElementsByClassName('text')[0].textContent = 'Ocurrió un error al eliminar la nota';
+                }
+
+                setTimeout(function(){ alert.style.display = "none"; }, 3600);
+                
+                load_info(createEventDelete); 
+            });
+        }
     }
-};
+    // CREATE EVENT ON CLICK TO DELETE ELEMENT
+
+    var close = document.getElementsByClassName("closebtn")[0];
+    close.addEventListener('click', function () {
+        var div = this.parentElement;
+        div.style.opacity = "0";
+        setTimeout(function(){ div.style.display = "none"; }, 600);
+    });
+})
+
+function isValidDate(dateString) {
+    var regEx = /^\d{4}-\d{2}-\d{2}$/;
+    if(!dateString.match(regEx)) return false;  // Invalid format
+    var d = new Date(dateString);
+    var dNum = d.getTime();
+    if(!dNum && dNum !== 0) return false; // NaN value, Invalid date
+    return d.toISOString().slice(0,10) === dateString;
+}
+
+function getDate() {
+    let date = new Date()
+
+    let day = String(date.getDate());
+    if(day.length == 1) {
+        day = '0' + day;
+    }
+
+    let month = String(date.getMonth() + 1);
+    if(month.length == 1) {
+        month = '0' + month;
+    }
+    
+    let year = date.getFullYear()
+
+    return `${day}/${month}/${year}`;
+}
